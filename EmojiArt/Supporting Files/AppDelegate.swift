126f907f31d38988c5ee45adf99b769efac145f4
Aleksandrs Poltarjonoks <codenator81@gmail.com>
2018-05-05 01:45:14 +0300

    Make it works without storyboard

---
+++ EmojiArt/Supporting Files/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EmojiArt
+//
+//  Created by Aleksandrs Poltarjonoks on 05/05/2018.
+//  Copyright © 2018 Aleksandrs Poltarjonoks. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)        
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
